0d2f1e85188e1762bded5bce24369ba758c5093c
ivar valen <ivar.valen@live.no>
2017-06-13 19:53:21 +0200

    Tutorial almost done, time to start Sandboxing

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="jumbotron text-center">
+     <div class="container">
+       <h1>Welcome to Laravel</h1>
+       <p>This is Laravel application from the Laravel from scratch</p>
+       <p><a class="btn btn-primary btn-lg" href="/" role="button">Learn more &raquo;</a> <a class="btn btn-success btn-lg" href="/" role="button">Register</a></p>
+     </div>
+   </div>
+
+
+
+@endsection
