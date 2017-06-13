4c88ddef3253953e493bd54ab36f4de8951d1c39
ivar valen <ivar.valen@live.no>
2017-06-13 21:37:14 +0200

    Setting up for tasks section

---
+++ database/migrations/2017_06_13_191239_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->mediumText('description')->nullable();
+            $table->boolean('completed');
+            $table->date('dueDate')->nullable();
+            $table->integer('createdBy_id');
+            $table->integer('assignedTo_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
